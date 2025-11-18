3290cb373af07954ef1be0a25ba43312120f64cc
duck-cpu <vmoreno@shinylotad.com>
2025-11-18 00:05:05 -0800

    exercise 1.3 complete

--- gcd.c
+++ gcd.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+//exercise 1.3 code
+typedef struct
+{
+    int numerator;
+    int denominator;
+} fraction;
+
+//prototype, too lazy to move simplify
+int gcd(int u, int v);
+
+fraction simplify(fraction *f)
+{
+    int common = gcd(f->numerator, f->denominator);
+
+    f->numerator /= common;
+    f->denominator /= common;
+}
+
 int gcd(int u, int v)
 {
     int t;
@@ -24,11 +42,19 @@ int gcd(int u, int v)
 
 int main()
 {
-    int x, y;
-    while (scanf("%d %d", &x, &y) != EOF)
+    fraction f;
+
+    while (scanf("%d/%d", &f.numerator, &f.denominator) != EOF)
+    {
+        simplify(&f);
+        printf("Your fraction reduced is: %d/%d\n", f.numerator, f.denominator);
+    }
+
+    /* while (scanf("%d %d", &x, &y) != EOF)
     {   
         //exercise 1.2 code
         // if (x>0 && y>0)
-        printf("X=%d Y=%d GCD=%d\n", x, y, gcd(x,y));
+        printf("X=%d Y=%d GCD=%d\n", x, y, gcd(x,y))
     }
+    */
 }
